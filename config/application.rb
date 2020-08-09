7d4dc6403e9095424c0c174d0f2b71df41b528f2
avmxmv <alexandmaximov@gmail.com>
2020-08-09 17:24:53 +0300

    Исправил недочёты

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Guru
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.time_zone = 'Moscow'
+    config.time_zone = 'Greenwich Mean Time (Moscow)'
+    I18n.default_locale = :ru
   end
 end
