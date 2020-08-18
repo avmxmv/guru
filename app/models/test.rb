2530ce10074a1111fb1d8a0c7f3c4454de1624fd
avmxmv <alexandmaximov@gmail.com>
2020-08-19 02:23:08 +0300

    fix 2

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.test_categories(category)
     category_find = Category.find_by(title: category)
-    Test.where(category_id: category_find.id).order(id: :DESC)
+    Test.where(category_id: category_find.id).order(id: :DESC).select(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   def user_tests(level)
-    test_ids = Result.where(user_id: id).select(:test_id)
-    Test.where(id: test_ids).where(level: level)
+    # test_ids = Result.where(user_id: id).select(:test_id)
+    # Test.where(id: test_ids).where(level: level)
+    Test.joins("LEFT JOIN results ON results.test_id = tests.id WHERE results.user_id = ? AND level = ?", self.id, level)
   end
 end
